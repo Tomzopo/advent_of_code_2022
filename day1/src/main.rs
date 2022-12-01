fb06633a5d623e50edb2af16207593960cdb7f4d
Thomas Stewart <thomas.stewart0494@gmail.com>
2022-12-01 12:16:24 +0200

    Day1

---
+++ day1/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let file = include_str!("../input/calories.txt");
+    let mut elves_summed_calories: Vec<i64> = file
+        .split("\r\n\r\n")
+        .map(|elf_calories| {
+            elf_calories.lines()
+                .map(|calorie| calorie.parse::<i64>().unwrap())
+                .sum()
+        })
+        .collect();
+
+    println!("Max Calories {}", elves_summed_calories.iter().max().unwrap());
+
+    elves_summed_calories.sort();
+    println!(
+        "Sum of top 3 Calories: {}",
+        elves_summed_calories.iter().rev().take(3).sum::<i64>()
+    );
+}
